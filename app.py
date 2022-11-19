5490be5df2df097535431d85a48a66d756d23478
msalbrain <salmanmusa2020@gmail.com>
2022-11-19 21:41:29 +0100

    made changes1

--- app.py
+++ app.py
@@ -2,6 +2,45 @@ from fastapi import FastAPI, Request, responses
 
 app = FastAPI()
 
+fake_db = {
+    "data":
+    [{
+        "name": "",
+        "phone": "902346829",
+        "activity": "",
+        "joined": 897643289,
+        "history": [
+            {
+                "date": 6781982376,
+                "complain": "",
+            }
+        ]
+    },
+        {
+            "name": "",
+            "activity": "",
+            "joined": 897643289,
+            "history": [
+                {
+                    "date": 6781982376,
+                    "complain": "",
+                }
+            ]
+        }, {
+        "name": "",
+        "activity": "",
+        "joined": 897643289,
+        "history": [
+            {
+                "date": 6781982376,
+                "complain": "",
+            }
+        ]
+    }
+
+    ]
+}
+
 
 @app.get("/test")
 def check():
@@ -16,12 +55,29 @@ async def index(request: Request):
     phone_number = form["phoneNumber"]
     text = form["text"]
 
-    if text == "":
-        return str("CON welcome\n please send 1")
-    elif text == "1":
-        return "END welldone you can read english"
+    user = {}
+
+
+
+    # if phone_number in fake_db.get("data"):
+    for i in fake_db.get("data"):
+        if i["phone"] == str(phone_number):
+            user = i
+            break
+
+    if user:
+        if text == "":
+            return str("CON welcome\n please send 1")
+        elif text == "1":
+            return "END welldone you can read english"
+        else:
+            return "END dimwit"
     else:
-        return "END dimwit"
+        return str("CON welcome\n what is your name")
 
 
-
+a = 0
+b = ""
+c = []
+d = {}
+e = ()
